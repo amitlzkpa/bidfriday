c82ce907e36d9c264e8533739d6f775efbdca24a
amitlzkpa <amit.lzkpa@gmail.com>
2020-08-10 11:23:25 -0500

    don't run ngrok on prod

--- ngrok.js
+++ ngrok.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
-const ngrok = require('ngrok');
-const clipboardy = require('clipboardy');
+if (process.env.NODE_ENV !== 'production') {
+  
+  require('dotenv').config();
+  const ngrok = require('ngrok');
+  const clipboardy = require('clipboardy');
+  
+  let PORT = 4001;
+  if (process.env.NODE_ENV === 'production') PORT = process.env.PORT || 8080;
+  
+  (async function() {
+    const url = await ngrok.connect(PORT);
+    console.log(`Copying URL to clipboard: ${url}`);
+    clipboardy.writeSync(url);
+  })();
+  
+}
 
-let PORT = 4001;
-if (process.env.NODE_ENV === 'production') PORT = process.env.PORT || 8080;
-
-(async function() {
-  const url = await ngrok.connect(PORT);
-  console.log(`Copying URL to clipboard: ${url}`);
-  clipboardy.writeSync(url);
-})();
