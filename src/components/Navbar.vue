7c77443303a58d19ae6c6683360013340e94f15d
amitlzkpa <amit.lzkpa@gmail.com>
2020-08-17 02:21:15 -0500

    update navbar with md-toolbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1>BidFriday</h1>
-    <a target="_blank" :href="'https://auth.monday.com/oauth2/authorize?client_id=74f5d4a266dec72194a44f947d25ce70&redirect_uri=' + redirect_uri + '/monday/connect'">Link</a>
-    <hr />
+    <md-toolbar>
+      <h3 class="md-title" style="flex: 1">BidFriday</h3>
+      <md-button target="_blank" :href="'https://auth.monday.com/oauth2/authorize?client_id=74f5d4a266dec72194a44f947d25ce70&redirect_uri=' + redirect_uri + '/monday/connect'" class="md-primary">Link</md-button>
+    </md-toolbar>
   </div>
 </template>
 
