f260e4c6c3d3af4ade25b379cfe9f2733c176e53
amitlzkpa <amit.lzkpa@gmail.com>
2020-08-16 19:00:28 -0500

    show latest tenderitem names

--- src/components/MondayRequestBoardView.vue
+++ src/components/MondayRequestBoardView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>    
+  <div>
     <button @click="refresh">Refresh</button>
 
     <table>
--- src/components/TenderView.vue
+++ src/components/TenderView.vue
@@ -3,6 +3,19 @@
 
     <h1>TenderView</h1>
 
+    <button @click="refresh">Refresh</button>
+
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+    <p>{{ priceRevealType }}</p>
+    <p>{{ mustBidOnAll }}</p>
+
+    <hr />
+
+    <p v-for="slot in slots" :key="slot._id">
+      {{ slot.tenderLineItems[slot.tenderLineItems.length - 1].name }}
+    </p>
+
   </div>
 </template>
 
@@ -12,6 +25,11 @@ export default {
   props: ['tenderId'],
   data () {
     return {
+      name: null,
+      description: null,
+      priceRevealType: null,
+      mustBidOnAll: false,
+      slots: []
     };
   },
   async mounted () {
@@ -27,6 +45,11 @@ export default {
       };
       let tData = await this.$api.post('/api/get-tender', postData);
       console.log(tData.data);
+      this.name = tData.data.name;
+      this.description = tData.data.description;
+      this.priceRevealType = tData.data.priceRevealType;
+      this.mustBidOnAll = tData.data.mustBidOnAll;
+      this.slots = tData.data.slots;
     }
   }
 }
