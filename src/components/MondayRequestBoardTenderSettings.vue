01576630cf552422e5351524fbfc73e7ad1d298b
amitlzkpa <amit.lzkpa@gmail.com>
2020-08-28 23:46:21 -0600

    relative homepage path

---
+++ src/components/MondayRequestBoardTenderSettings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  async mounted() {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
