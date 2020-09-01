5e91652c75259989f50c35ff3c9c51f4155aa929
amitlzkpa <amit.lzkpa@gmail.com>
2020-08-31 22:44:13 -0500

    home page update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,53 +22,174 @@
       </div>
       
     </div>
-    
+
+
+    <hr />
+
+
+
+
+
+
     <div class="md-layout md-gutter">
-      <div class="md-layout-item"></div>
+
       <div class="md-layout-item">
-        <div style="text-align: center;">
-
-          <p class="md-display-2">BidFriday</p>
-          <span class="md-subheading">Manage your bids in a simple friendly way.</span>
-
-          <br /><br />
-
-          <span class="md-title">Short explainer</span>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/lR2XPe2B-pU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <br />
-
-          <span class="md-title">Long explainer</span>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/wKc1ABeGOiQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-          <br /><br />
-
-          <img src="" />
-          <span class="md-subheading">Upload your item list.</span>
-
-          <br /><br />
-
-          <img src="" />
-          <span class="md-subheading">Share and invite vendors to bid.</span>
-
-          <br /><br />
-
-          <img src="" />
-          <span class="md-subheading">Easily manage changes to your requirements and bids.</span>
-
-          <br /><br />
-
-          <img src="" />
-          <span class="md-subheading">Negotiate prices to make sure you get the best deal.</span>
-
-          <br /><br />
-
-          <img src="" />
-          <span class="md-subheading">Look back at your previous bids and develop insights into your vendor network.</span>
-
+        <div>
+          <md-content style="text-align: center; padding: 100px 0px 100px 100px;">
+            <p class="md-display-1">
+              BidFriday helps you coordinate your bidding process.
+              <br /><br />
+              Upload your requirements to a Monday board and invite use BidFriday to invite vendors to bid for fulfilling your requests.
+              <br /><br />
+              BidFriday takes care of aggregating their responses and presents you with a tidy dashboard with all the info.
+            </p>
+          </md-content>
         </div>
       </div>
-      <div class="md-layout-item"></div>
+      
+      <div class="md-layout-item">
+        <div style="text-align: center; padding: 200px 0px 200px 0px;">
+          <img src="/imgs/001_bidslot-details.png" style="width: 600px;" class="shadow" />
+        </div>
+      </div>
+      
     </div>
+
+
+
+    <hr width="300px;" />
+
+
+
+    <div class="md-layout md-gutter">
+      
+      <div class="md-layout-item">
+        <div style="text-align: center; padding: 200px 100px 200px 100px;">
+          <img src="/imgs/001_bid-updated02.png" style="width: 600px;" class="shadow" />
+        </div>
+      </div>
+
+      <div class="md-layout-item">
+        <div>
+          <md-content style="text-align: center; padding: 100px 100px 100px 0px;">
+            <p class="md-display-1">
+              Present your vendors with a simple format to submit their bids in a format convenient for everyone.
+              <br /><br />
+              BidFriday highlights the information that changes as the project progress to make it easy for everyone to read.
+              <br /><br />
+              No more getting lost in long email chains.
+            </p>
+          </md-content>
+        </div>
+      </div>
+      
+    </div>
+
+
+
+    <hr width="300px;" />
+
+
+
+    <div class="md-layout md-gutter">
+
+      <div class="md-layout-item">
+        <div>
+          <md-content style="text-align: center; padding: 100px 0px 100px 100px;">
+            <p class="md-display-1">
+              View aggregated information from bids submitted for your request across the board.
+              <br /><br />
+              Develop useful insights about product prices and vendor networks from a glance.
+              <br /><br />
+              Use this information to get the prices for your needs.
+            </p>
+          </md-content>
+        </div>
+      </div>
+      
+      <div class="md-layout-item">
+        <div style="text-align: center; padding: 200px 0px 200px 0px;">
+          <img src="/imgs/001_tender-overview.png" style="width: 600px;" class="shadow" />
+        </div>
+      </div>
+      
+    </div>
+
+
+
+    <hr width="300px;" />
+
+
+
+    <div class="md-layout md-gutter">
+      
+      <div class="md-layout-item">
+        <div style="text-align: center; padding: 200px 100px 200px 100px;">
+          <img src="/imgs/001_tender-edit.png" style="width: 600px;" class="shadow" />
+        </div>
+      </div>
+
+      <div class="md-layout-item">
+        <div>
+          <md-content style="text-align: center; padding: 100px 100px 100px 0px;">
+            <p class="md-display-1">
+              Manage your item list from a single place. Updates are synced automatically across all your vendors.
+              <br /><br />
+              Use the automation features from the Monday apps ecosystem to steamline your workflows.
+              <br /><br />
+              No more hunting for emails to inform vendors about updates.
+            </p>
+          </md-content>
+        </div>
+      </div>
+      
+    </div>
+
+
+
+    <hr width="300px;" />
+
+
+
+
+
+    <div style="height: 100px"></div>
+
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item" style="text-align: center;">
+        <span class="md-display-1">See it in action</span>
+      </div>
+    </div>
+
+    
+    <div style="height: 50px"></div>
+
+
+    <div class="md-layout md-gutter">
+
+      <div class="md-layout-item" style="text-align: center;">
+        <span class="md-title">Short explainer</span>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/lR2XPe2B-pU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      
+      <div class="md-layout-item" style="text-align: center;">
+        <span class="md-title">Long explainer</span>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/wKc1ABeGOiQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+
+      <div class="md-layout-item" style="text-align: center;">
+        <a href="https://auth.monday.com/oauth2/authorize?client_id=74f5d4a266dec72194a44f947d25ce70&response_type=install"> <img alt="Add to monday.com" height="42" src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/uploads/Tal/4b5d9548-0598-436e-a5b6-9bc5f29ee1d9_Group12441.png" /> </a>
+      </div>
+
+    </div>
+
+
+    <div style="height: 200px"></div>
+
+
+
+
+
   </div>
 </template>
 
@@ -78,4 +199,9 @@ export default {
 </script>
 
 <style scoped>
+.shadow {
+  -webkit-box-shadow: 0px 0px 24px -11px rgba(0,0,0,0.76);
+  -moz-box-shadow: 0px 0px 24px -11px rgba(0,0,0,0.76);
+  box-shadow: 0px 0px 24px -11px rgba(0,0,0,0.76);
+}
 </style>
